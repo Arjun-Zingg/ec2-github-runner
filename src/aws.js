ce215b6e5bb0e3cf7261185e4c5e5f805d08e14f
nitish <nitish@zingg.ai>
2025-02-05 18:42:02 +0530

    BlockDeviceMappings Added

--- src/aws.js
+++ src/aws.js
@@ -62,6 +62,17 @@ async function startEc2Instance(label, githubRegistrationToken) {
     IamInstanceProfile: { Name: config.input.iamRoleName },
     TagSpecifications: config.tagSpecifications,
     InstanceMarketOptions: buildMarketOptions(),
+    BlockDeviceMappings: [
+                           {
+                             "DeviceName": "/dev/sda1",
+                             "Ebs": {
+                               "DeleteOnTermination": true,
+                               "SnapshotId": "snap-0cea39e80265a11bf",
+                               "VolumeSize": 256,
+                               "VolumeType": "gp3"
+                             }
+                           }
+                         ]
   };
 
   try {
